68a51db87d9e6ceb993a1138b925eb0c9d0eb1ef
Gerasim Gerasimov <dialix@yandex.ru>
2019-07-07 15:06:31 +0700

    выделил в компонент

--- src/App.vue
+++ src/App.vue
@@ -1,96 +1,43 @@
-//http://qaru.site/questions/14357074/how-to-get-the-element-width-and-height-immediately-when-it-resizing-in-vuejs
-//выше ссылка про refs и наблюдатели
-
-//ссылка на пример http://jsfiddle.net/Vlad_IT/qt64rouL/1/
-//Чтобы стилизовать инлайн-svg, я присвоил ему id (id="sample" )
-//и теперь в файле стилей могу через #sample воздействовать на него
-//например #sapmle rect {} воздействует на svg->rect
+//Если требуется реагировать на каждую передачу в props то надо передавать объект
 <template>
-  <div id="app" class="wrapper" ref="loadprogresser" @resize='onResize'>
-    <svg id="sample" version="1.1" xmlns="http://www.w3.org/2000/svg"
-          width="100%" height="100%">
-        <g :transform="transform">
-          <path :d="d"/>
-        </g>
-        <text x="50%" y="50%" dominant-baseline="central" text-anchor="middle">{{TextPrc}}</text>
-    </svg>
+  <div>
+    <h2>Progresser</h2>
+    <progresser
+      class="progresser"
+      :maxSamples = "SamplesInProgresser"
+      :Point = "Point"
+    ></progresser>          
   </div>
 </template>
 
 <script>
+import Progresser from "./components/Progresser"
 export default {
   name: 'app',
   data(){
     return {
-      d:"M 0 0",
-      Samples:0,//номер текущего сэмпла
-      maxSamples:400, //кол-во сэмплов в 100%-ах ширины
-      wrapHeight:100,//высота в которую надо вписать изображение
-      maxY:0,
-      scales:{
-        w:1, h:1
-      },
-      timePoints:[],
+      SamplesInProgresser:400,
       TimeM:100,
-      transform:'scale( 1, -1) translate(0,0) rotate(0)',
-      TextPrc: '0%'
+      Point:{value:0},
+      index:0
     }
-
   },
-  mounted: function(){
-    this.getScales()
+  created: function () {
     this.updateStar()
   },
   methods:{
-    getScales(){
-      console.log('getScale')
-      const {width, height} = this.$refs.loadprogresser.getBoundingClientRect()
-      this.scales.w = width / this.maxSamples
-      this.wrapHeight = height
-      console.log(this.scales.w, width, this.maxSamples)
-      this.transform = `scale( 1, -1) translate(0,${-this.wrapHeight}) rotate(0)`
-    },
-    onResize(){
-
-    },
-    getVScale(){
-      this.scales.h = (this.maxY == 0)? 0 : this.wrapHeight / this.maxY
-    },
-    //передаю значения выделяю максимум и выдаю шкалу относительно этого максимума
-    getYMax({value = 0}){
-      if (value > this.maxY) this.maxY = value
-    },
-    addTime({time = 0}){
-      if (this.Samples < this.maxSamples) {
-        this.getYMax({value:time})
-        this.getVScale()
-        this.timePoints.push(time) //интересуют Int
-        this.Samples ++;
-        this.TextPrc = `${((this.Samples * 100)/this.maxSamples) | 0} %`
-      }
-    },
-    getSVGTimePoints(){
-      //теперь создаю строку для Path
-      let predY = 0
-      let path = this.timePoints.reduce((str, item)=>{
-        let dY = (item - predY) * this.scales.h
-        predY = item
-        return str + `l ${this.scales.w} ${dY} `
-      },'M 0 0 ')
-      path +=`L ${this.timePoints.length * this.scales.w} 0 `// Z` контур можно не замыкать
-      return path
-    },
     updateStar(){
-      this.addTime({time:(this.TimeM*Math.random() | 0)})
-      this.TimeM *= 1.01
-      this.d = this.getSVGTimePoints()
-      setTimeout(this.updateStar, 1)
-    }
+      if (this.index < this.SamplesInProgresser) {
+        this.index++;
+        this.Point = {value:(this.TimeM*Math.random() | 0)}
+        this.TimeM *= 1.01
+        setTimeout(this.updateStar, 1)
+      }
+    }    
   },
-  computed:{
 
-  },  
   components: {
+    Progresser
   }
 }
 </script>
@@ -98,32 +45,11 @@ export default {
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 
-#sample path {
-  opacity: 0.5;
-	fill: lightgreen;
-	stroke: green;
-	stroke-width: 1;
-	transition: all 350ms;
+.progresser {
+  width: 300px;
+  height: 80px; 
 }
-
-#sample text {
-  opacity: 0.5;
-} 
-
-.wrapper {
-  font-size: 64px;
-  border-left: 1px gray solid;
-  border-right: 1px gray solid;
-  width: 200px;
-  height: 150px;
-  overflow: hidden;
-}
-
 </style>
---
+++ src/components/Progresser.vue
@@ -0,0 +1,121 @@
+//http://qaru.site/questions/14357074/how-to-get-the-element-width-and-height-immediately-when-it-resizing-in-vuejs
+//выше ссылка про refs и наблюдатели
+
+//ссылка на пример http://jsfiddle.net/Vlad_IT/qt64rouL/1/
+//Чтобы стилизовать инлайн-svg, я присвоил ему id (id="sample" )
+//и теперь в файле стилей могу через #sample воздействовать на него
+//например #sapmle rect {} воздействует на svg->rect
+<template>
+  <div id="app" class="wrapper" ref="loadprogresser">
+    <svg id="sample" version="1.1" xmlns="http://www.w3.org/2000/svg"
+          width="100%" height="100%">
+        <g :transform="transform">
+          <path :d="getPath"/>
+        </g>
+        <text x="50%" y="50%" dominant-baseline="central" text-anchor="middle">{{TextPrc}}</text>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  props:{
+    maxSamples: {//кол-во сэмплов в 100%-ах ширины
+      type: Number,
+      default: 400
+    },
+    Point:{
+      value:0,
+      index:0
+    }
+  },
+  data(){
+    return {
+      Samples:0,//номер текущего сэмпла
+      wrapHeight:100,//высота в которую надо вписать изображение
+      maxY:0,//максимальная по Y величина (для вертикального масштабирования)
+      scales:{
+        w:1, h:1
+      },
+      Points:[],
+      transform:'scale( 1, -1) translate(0,0) rotate(0)',
+      TextPrc: '0%'
+    }
+
+  },
+  mounted: function(){
+    this.getScales()
+  },
+  methods:{
+    getScales(){
+      const {width, height} = this.$refs.loadprogresser.getBoundingClientRect()
+      this.scales.w = width / this.maxSamples
+      this.wrapHeight = height
+      console.log(this.scales.w, width, this.maxSamples)
+      this.transform = `scale( 1, -1) translate(0,${-this.wrapHeight}) rotate(0)`
+    },
+    getVScale(){
+      this.scales.h = (this.maxY == 0)? 0 : this.wrapHeight / this.maxY
+    },
+    //передаю значения выделяю максимум и выдаю шкалу относительно этого максимума
+    getYMax({value = 0}){
+      this.maxY = (value > this.maxY)
+        ? value
+        : this.maxY 
+    },
+    addValue({value = 0}){
+      if (this.Samples < this.maxSamples) {
+        this.getYMax({value})
+        this.getVScale()
+        this.Points.push(value) //интересуют Int
+        this.Samples ++;
+        this.TextPrc = `${((this.Samples * 100)/this.maxSamples) | 0} %`
+      }
+    },
+    getSVGPoints(){
+      //теперь создаю строку для Path
+      let predY = 0
+      let path = this.Points.reduce((str, item)=>{
+        let dY = (item - predY) * this.scales.h
+        predY = item
+        return str + `l ${this.scales.w} ${dY} `
+      },'M 0 0 ')
+      path +=`L ${this.Points.length * this.scales.w} 0 `// Z` контур можно не замыкать
+      return path
+    },
+  },
+  computed:{
+    getPath(){
+      this.addValue({value:this.Point.value})
+      return this.getSVGPoints()//this.d
+    }
+  },  
+  components: {
+  }
+}
+</script>
+
+<style>
+
+.wrapper {
+  width: 400px;/* размеры задаются в стилях прложения*/
+  height: 100px;/*встроенные стили (поумолчанию) перекрываются стилями приложения*/
+  font-size: 4rem;
+  font-weight: 600;
+  border-left: 1px gray solid;
+  border-right: 1px gray solid;
+  overflow: hidden;
+}
+
+#sample path {
+  opacity: 0.5;
+	fill: lightgreen;
+	stroke: green;
+	stroke-width: 1;
+}
+
+#sample text {
+  opacity: 0.5;
+} 
+</style>
